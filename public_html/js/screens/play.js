e2a186753b08b6d87a817decac014895140bac9f
alxrico <alexrico403@gmail.com>
2014-04-28 14:17:04 -0700

    new
    new code and levels

--- public_html/js/screens/play.js
+++ public_html/js/screens/play.js
@@ -6,10 +6,9 @@ game.PlayScreen = me.ScreenObject.extend({
 		// reset the score
 		game.data.score = 0;
                 
-                me.levelDirector.loadLevel("newworld");
+                me.levelDirector.loadLevel("1");
           
-              var player = new game.PlayerEntity(0, 420, {});
-              me.game.add(player, this.z);
+                this.resetPlayer(0, 420);
               
 		// add our HUD to the game world
 		this.HUD = new game.HUD.Container();
@@ -23,5 +22,10 @@ game.PlayScreen = me.ScreenObject.extend({
 	onDestroyEvent: function() {
 		// remove the HUD from the game world
 		me.game.world.removeChild(this.HUD);
-	}
+	},
+                
+        resetPlayer: function(x, y) {
+               var player = me.pool.pull("player", x, y, {});
+               me.game.world.addChild(player, 7);
+        }
 });
